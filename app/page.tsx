f9d1671140e4744811ee34a68d0b63135888d27b
Akash Soedamah <akash.soedamah@gmail.com>
2026-04-09 17:53:20 +0100

    fix random boilerplate NextJS image warning

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,15 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
+        <div className="relative h-5 w-[100px]">
+          <Image
+            className="dark:invert object-contain"
+            src="/next.svg"
+            alt="Next.js logo"
+            fill
+            priority
+          />
+        </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
@@ -41,13 +42,14 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
+            <div className="relative h-4 w-4">
+              <Image
+                className="dark:invert object-contain"
+                src="/vercel.svg"
+                alt="Vercel logomark"
+                fill
+              />
+            </div>
             Deploy Now
           </a>
           <a
