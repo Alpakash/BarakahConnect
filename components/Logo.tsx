9590be5a1c4430b39b24a5229367ac1326444abf
Akash Soedamah <akash.soedamah@gmail.com>
2026-04-23 09:45:09 +0200

    style: replace JPG logo with transparent SVG logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,11 +3,11 @@ import Image from 'next/image'
 export default function Logo({ className }: { className?: string }) {
   return (
     <Image 
-      src="/barakah-connect-logo.jpg" 
+      src="/barakah-connect.svg" 
       alt="Barakah Connect" 
       width={400} 
       height={400} 
-      className={`${className} mix-blend-multiply object-contain`} 
+      className={`${className} object-contain`} 
       priority
     />
   )
